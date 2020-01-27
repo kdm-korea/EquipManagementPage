a39fa3b2c81ed082994bcd41924f3ae8993c5d2f
KDM_korea <kdm_korea@naver.com>
2020-01-27 22:37:59 +0900

    Feat: Jpa Find method add

--- SC/src/main/java/com/services/webservice/domain/UserRepository.java
+++ SC/src/main/java/com/services/webservice/domain/UserRepository.java
@@ -1,7 +1,10 @@
 package com.services.webservice.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	List<User> findByName(String name);
+	List<User> findByStudentNum(Long studentNum);
 }
