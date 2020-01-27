2614202b05ae6e6ce96c48adf4f866d62e5b79ef
KDM_korea <kdm_korea@naver.com>
2020-01-27 15:56:54 +0900

    Feat: modified calss name

--- SC/src/test/java/com/services/webservice/domain/UserRepositoryTest.java
+++ SC/src/test/java/com/services/webservice/domain/UserRepositoryTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UserRepositoryTest {
 	
 	@Autowired
-	private UsersRepository userRepository;
+	private UserRepository userRepository;
 	
 	@AfterAll
 	public void cleanUp() {
