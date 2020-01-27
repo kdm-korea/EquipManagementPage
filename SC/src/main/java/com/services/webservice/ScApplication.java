4dad0cd21139758228a8bea8ebf354d97efa7da1
KDM_korea <kdm_korea@naver.com>
2020-01-27 15:11:00 +0900

    Feat: Main application class

---
+++ SC/src/main/java/com/services/webservice/ScApplication.java
@@ -0,0 +1,13 @@
+package com.services.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScApplication.class, args);
+	}
+
+}
