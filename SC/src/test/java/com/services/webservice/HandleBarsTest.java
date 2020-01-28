0133e5ec4491640cacb4eae3b1cff59f9d9c96d7
KDM_korea <kdm_korea@naver.com>
2020-01-28 22:38:46 +0900

    Test: Handlebars test

---
+++ SC/src/test/java/com/services/webservice/HandleBarsTest.java
@@ -0,0 +1,29 @@
+package com.services.webservice;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+//@TestInstance(Lifecycle.PER_CLASS)
+public class HandleBarsTest {
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+	
+	@Test
+	public void pageLoading() {
+		String body = this.testRestTemplate.getForObject("/", String.class);
+		
+		assertThat(body).contains("fdsa");
+	}
+}
