450e3da4988bce2c0da37f34e86e4b56e5292bcb
KDM_korea <kdm_korea@naver.com>
2020-01-28 22:48:21 +0900

    Feat: EquipState Entity class

---
+++ SC/src/main/java/com/services/webservice/domain/EquipState.java
@@ -0,0 +1,32 @@
+package com.services.webservice.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "EQUIPSTATE")
+public class EquipState {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(nullable = false)
+	private String state;
+
+	@Builder
+	public EquipState(Long id, String state) {
+		this.id = id;
+		this.state = state;
+	}
+}
