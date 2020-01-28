e36f99f7c5e27740850d9ae0a57c1e7821351c0e
KDM_korea <kdm_korea@naver.com>
2020-01-28 22:49:59 +0900

    Feat: Qualifition jpa interface

---
+++ SC/src/main/java/com/services/webservice/domain/QualifitionRepository.java
@@ -0,0 +1,7 @@
+package com.services.webservice.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QualifitionRepository extends JpaRepository<Qualifition, Long>{
+
+}
