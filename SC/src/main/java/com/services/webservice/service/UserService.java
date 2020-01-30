ce60ad8028bb488f23befffb41aac849080c9441
KDM_korea <kdm_korea@naver.com>
2020-01-30 16:31:04 +0900

    Feat: Service logic add

---
+++ SC/src/main/java/com/services/webservice/service/UserService.java
@@ -0,0 +1,31 @@
+package com.services.webservice.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.services.webservice.domain.User;
+import com.services.webservice.domain.UserRepository;
+import com.services.webservice.service.dto.UserLoginDto;
+import com.services.webservice.service.dto.UserSignUpDto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class UserService {
+	
+	private UserRepository userRepository;
+	
+	@Transactional()
+	public ApiResponse login(UserLoginDto dto) {
+		//TODO: 抗寇贸府
+		return new ApiResponse(200, "Success", userRepository.findByStudentNum(dto.toEntity().getStudentNum())); 
+	}
+	
+	@Transactional
+	public ApiResponse signUp(UserSignUpDto dto) {
+		//TODO: 抗寇贸府
+		return new ApiResponse(200, "SignUp Success", userRepository.save(dto.toEntity()));
+	}
+}
