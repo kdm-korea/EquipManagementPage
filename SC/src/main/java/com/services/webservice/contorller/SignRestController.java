501fd8eebfe91af3f97ab07bd83824b739aa266e
KDM_korea <kdm_korea@naver.com>
2020-01-30 16:30:49 +0900

    Feat: restController signup, login

---
+++ SC/src/main/java/com/services/webservice/contorller/SignRestController.java
@@ -0,0 +1,29 @@
+package com.services.webservice.contorller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.services.webservice.service.ApiResponse;
+import com.services.webservice.service.UserService;
+import com.services.webservice.service.dto.UserLoginDto;
+import com.services.webservice.service.dto.UserSignUpDto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+public class SignRestController {
+
+	private UserService userService;
+	
+	@PostMapping("/Login")
+	public ApiResponse login(@RequestBody UserLoginDto loginDto) {
+		return userService.login(loginDto);
+	}
+	
+	@PostMapping("/signUp")
+	public ApiResponse signUp(@RequestBody UserSignUpDto signUpDto) {
+		return userService.signUp(signUpDto);
+	}
+}
