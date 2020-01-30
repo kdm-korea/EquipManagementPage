20f2372bf081c8af46bf959354c911d62505e572
KDM_korea <kdm_korea@naver.com>
2020-01-30 16:29:53 +0900

    Feat: SingUp dto

---
+++ SC/src/main/java/com/services/webservice/service/dto/UserSignUpDto.java
@@ -0,0 +1,33 @@
+package com.services.webservice.service.dto;
+
+import javax.persistence.Column;
+
+import com.services.webservice.domain.User;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserSignUpDto {
+
+	private Long id;
+	
+	private Long studentNum;
+
+	private String name;
+
+	private String phoneNumber;
+	
+	public User toEntity() {
+		return User.builder()
+				.id(id)
+				.studentNum(studentNum)
+				.name(name)
+				.phoneNumber(phoneNumber)
+				.build();
+	}
+}
