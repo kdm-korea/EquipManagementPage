d31fa639c43da696ec39084eeb2abf0e2c21e33e
KDM_korea <kdm_korea@naver.com>
2020-01-30 16:30:21 +0900

    Feat: post apiResponse

---
+++ SC/src/main/java/com/services/webservice/service/ApiResponse.java
@@ -0,0 +1,18 @@
+package com.services.webservice.service;
+
+public class ApiResponse {
+	
+	private int status;
+	private String message;
+	private Object result;
+	
+	public ApiResponse(int status, String message, Object result) {
+		this.status = status;
+		this.message = message;
+		this.result = result;
+	}
+	
+	public int getStatus() {
+		return status;
+	}
+}
