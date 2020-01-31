b9c06e6507174e2341ca4f38bede5936f3afbe03
KDM_korea <kdm_korea@naver.com>
2020-01-31 23:05:03 +0900

    Feat: signUp post js

---
+++ SC/src/main/resources/static/js/signUp.js
@@ -0,0 +1,29 @@
+var signUp = {
+		init : function() {
+			var _this = this;
+			$('#btn-signup').on('click', function () {
+				_this.save();
+			});
+		},
+		save : function() {
+			var data = {
+					name: $('#name').val(),
+					studentNum: $('#studentNum').val(),
+					phoneNumber: $('#phoneNum').val()
+			};
+			$.ajax({
+				type: 'POST',
+				url: '/signup',
+				dataType: 'json',
+				contentType:'application/json; charset=utf-8',
+				data: JSON.stringify(data)
+			}).done(function(){
+				alert('글이 등록되었습니다.');
+				location.reload();
+			}).fail(function (error){
+				alert('error');
+			});
+		}
+};
+
+signUp.init();
