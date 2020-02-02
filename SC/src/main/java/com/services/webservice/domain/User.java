ecf7b8df5f9aa21979be78d13a222ec7d601b025
KDM_korea <kdm_korea@naver.com>
2020-02-02 15:06:40 +0900

    Feat: role class join

--- SC/src/main/java/com/services/webservice/domain/User.java
+++ SC/src/main/java/com/services/webservice/domain/User.java
@@ -1,9 +1,16 @@
 package com.services.webservice.domain;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -30,11 +37,17 @@ public class User extends BaseTimeEntity{
 	@Column(nullable = true)
 	private String phoneNumber;
 
+	@ManyToMany(targetEntity = Role.class, fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(foreignKey = @ForeignKey(name = "fk_user_to_role"))
+	private Set<Role> roles;
+
 	@Builder
-	public User(Long id, long studentNum, String name, String phoneNumber) {
+	public User(Long id, String studentNum, String password, String name, String phoneNumber, Set<Role> roles) {
 		this.id = id;
 		this.studentNum = studentNum;
+		this.password = password;
 		this.name = name;
 		this.phoneNumber = phoneNumber;
+		this.roles = roles;
 	}
 }
