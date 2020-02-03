79a861691b6342c397605f1618cfc1b03b5303c3
KDM_korea <kdm_korea@naver.com>
2020-02-03 21:51:22 +0900

    Feat: Security Config

--- SC/src/main/java/com/services/webservice/Security/SecurityConfig.java
+++ SC/src/main/java/com/services/webservice/Security/SecurityConfig.java
@@ -1,21 +1,73 @@
 package com.services.webservice.Security;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.header.writers.frameoptions.WhiteListedAllowFromStrategy;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.services.webservice.service.UserService;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private UserService userService;
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception{
-		//web.ignoring().antMatchers(method)
+		//항상 통과되어야 하는 목록들
+		web.ignoring().antMatchers("/static/**", "/css/**", "/img/**", "/js/**");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		//http.authorizeRequests()
-			//.antMatchers(method)
+		http.authorizeRequests()
+			//페이지 권한설정
+//				.antMatchers("/admin/**").hasRole("ADMIN")
+//				.antMatchers("/user/myinfo").hasRole("MEMBER")
+				//.antMatchers("/h2").permitAll()
+				.antMatchers("/**").permitAll()
+			.and()
+				.formLogin()
+				.loginPage("/")
+				.usernameParameter("loginStudentNum")
+				.passwordParameter("loginPassword")
+				.defaultSuccessUrl("/user/test")
+				.failureUrl("/")
+				.permitAll()
+			.and()
+				.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+				.logoutSuccessUrl("/user/logout/result")
+				.invalidateHttpSession(true)
+			.and()
+				.exceptionHandling().accessDeniedPage("/user/denied");
+		
+		http.csrf().disable();
+
+		http.headers().frameOptions().disable();
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
 	}
 }
