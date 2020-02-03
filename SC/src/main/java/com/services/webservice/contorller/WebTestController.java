5d9266cd464cd7d412ad2aace168d645496c6d92
KDM_korea <kdm_korea@naver.com>
2020-02-03 21:47:20 +0900

    Feat: test controller

--- SC/src/main/java/com/services/webservice/contorller/WebTestController.java
+++ SC/src/main/java/com/services/webservice/contorller/WebTestController.java
@@ -1,16 +1,45 @@
 package com.services.webservice.contorller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.services.webservice.service.UserService;
+import com.services.webservice.service.dto.UserSignUpDto;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Controller
 public class WebTestController {
-	
+
 	@GetMapping("/")
 	public String main() {
 		return "index";
+	}
+
+	// 로그인 성공 페이지
+	@GetMapping("/user/test")
+	public String loginIn() {
+		return "test";
+	}
+
+	// 접근 거부 페이지
+	@GetMapping("/user/denied")
+	public String dispDenied() {
+		return "/denied";
+	}
+
+	// 내 정보 페이지
+	@GetMapping("/user/info")
+	public String dispMyInfo() {
+		return "/myinfo";
+	}
+
+	// 어드민 페이지
+	@GetMapping("/admin")
+	public String dispAdmin() {
+		return "/admin";
 	}
 }
