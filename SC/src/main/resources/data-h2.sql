42a6ca6a7d84b8b8de9fa6994e32f7b0d48bb0bc
KDM_korea <kdm_korea@naver.com>
2020-02-08 05:45:55 +0900

    Fix: Query modified for table structer change

--- SC/src/main/resources/data-h2.sql
+++ SC/src/main/resources/data-h2.sql
@@ -1,2 +1,3 @@
 insert into role (id, role) values(0,'ADMIN');
 insert into role (id, role) values(1,'MEMBER');
+INSERT INTO MEMBER ("ID","CREATED_DATE","MODIFIED_DATE","NAME","PASSWORD","PHONE_NUMBER","STUDENT_NUM","ROLE_ID_ID")VALUES(0,NOW(),NOW(),'김동민','a','01027703018','2019631001',0)
