bb1c2c0b9f3c548df13e79a45567cc67fe631a2b
KDM_korea <kdm_korea@naver.com>
2020-02-07 19:05:00 +0900

    Feat: Student ID check controller  for signUp

--- SC/src/main/java/com/services/webservice/contorller/SignRestController.java
+++ SC/src/main/java/com/services/webservice/contorller/SignRestController.java
@@ -2,11 +2,13 @@ package com.services.webservice.contorller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.services.webservice.service.ApiResponse;
 import com.services.webservice.service.UserService;
 import com.services.webservice.service.dto.UserSignUpDto;
+import com.services.webservice.service.dto.UserStudentNumChkDto;
 
 import lombok.AllArgsConstructor;
 
@@ -15,9 +17,17 @@ import lombok.AllArgsConstructor;
 public class SignRestController {
 
 	private UserService userService;
-	
+
 	@PostMapping("/signup")
+	@ResponseBody
 	public ApiResponse signUp(@RequestBody UserSignUpDto signUpDto) {
 		return userService.signUp(signUpDto);
+	}
+
+	@PostMapping("/idChk")
+	@ResponseBody
+	public ApiResponse studentNumChk(@RequestBody UserStudentNumChkDto dto) {
+		System.out.println(dto.getStudentNum());
+		return userService.studentNumChk(dto);
 	}
 }
