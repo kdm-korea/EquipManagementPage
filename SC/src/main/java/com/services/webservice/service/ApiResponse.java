e7863ba448aef5a4eecf3e91f2181d153c9b8ac8
KDM_korea <kdm_korea@naver.com>
2020-02-07 19:07:58 +0900

    Fix: ApiResponse class fix getter annotation

--- SC/src/main/java/com/services/webservice/service/ApiResponse.java
+++ SC/src/main/java/com/services/webservice/service/ApiResponse.java
@@ -1,18 +1,19 @@
 package com.services.webservice.service;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class ApiResponse {
-	
+
 	private int status;
 	private String message;
 	private Object result;
-	
+
 	public ApiResponse(int status, String message, Object result) {
 		this.status = status;
 		this.message = message;
 		this.result = result;
-	}
-	
-	public int getStatus() {
-		return status;
 	}
 }
