54291410b9a9c435364f6fbc57375e1a5cc32680
KDM_korea <kdm_korea@naver.com>
2020-02-10 05:49:51 +0900

    Fix: var -> const, let

--- SC/src/main/resources/static/js/signUp.js
+++ SC/src/main/resources/static/js/signUp.js
@@ -1,23 +1,16 @@
-// var token = $("meta[name='_csrf']").attr("content");
-// var header = $("meta[name='_csrf_header']").attr("content");
-//			
-// $.ajax (function() {
-// $(document).ajaxSend(function(e, xhr, options) {
-// xhr.setRequestHeader(header, token);
-// }//,
-// success : function(data, status, xhr){
-// alert(status);
-// },
-// error : function(data, status, xhr){
-// alert(status);
-// },
-// complete : function(data, status, xhr){
-// alert(status);
-// });
-// });
-var signUp = {
+//$(function() {
+//	var token = $("meta[name='_csrf']").attr("content");
+//	var header = $("meta[name='_csrf_header']").attr("content");
+//	$(document).ajaxSend(function(e, xhr, options) {
+//		if (token && header) {
+//			xhr.setRequestHeader(header, token);
+//		}
+//	});
+//});
+
+const signUp = {
 	init : function() {
-		var _this = this;
+		const _this = this;
 
 		$('#btn-signup').on('click', function() {
 			_this.chkStudentNum();
@@ -25,19 +18,19 @@ var signUp = {
 	},
 
 	chkStudentNum : function() {
-		var jsonData = {
+		const jsonData = {
 			studentNum : $('#studentNum').val()
 		};
 
 		$.ajax({
 			type : 'POST',
-			url : '/idChk',
 			dataType : 'json',
 			contentType : 'application/json; charset-utf-8',
+			url : '/idChk',
 			data : JSON.stringify(jsonData)
 		}).done(function(data, status, jqXHR) {
-			
-			if(data.message == "Success"){
+
+			if (data.message == "Success") {
 				signUp.save();
 			} else {
 				alert('이미 가입한 이력이 있는 학번입니다.');
@@ -47,9 +40,8 @@ var signUp = {
 		});
 	},
 
-	
 	save : function() {
-		var jsonData = {
+		const jsonData = {
 			name : $('#name').val(),
 			studentNum : $('#studentNum').val(),
 			phoneNumber : $('#phoneNum').val(),
@@ -62,7 +54,7 @@ var signUp = {
 			dataType : 'json',
 			contentType : 'application/json; charset=utf-8',
 			data : JSON.stringify(jsonData)
-		}).done(function(data, status, jqXHR){
+		}).done(function(data, status, jqXHR) {
 			console.log(data);
 			alert('가입이 완료되었습니다.');
 			location.reload();
