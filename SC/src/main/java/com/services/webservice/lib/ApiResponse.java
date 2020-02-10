fb145f964fc40ee434d38de2f8ffb8a3fb74404c
KDM_korea <kdm_korea@naver.com>
2020-02-11 05:51:54 +0900

    Fix: Modified directory structer & class name

--- SC/src/main/java/com/services/webservice/lib/ApiResponse.java
+++ SC/src/main/java/com/services/webservice/lib/ApiResponse.java
@@ -1,17 +1,17 @@
-package com.services.webservice.service;
+package com.services.webservice.lib;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
-public class ApiResponse {
+public class ApiResponse<T> {
 
 	private int status;
 	private String message;
 	private Object result;
 
-	public ApiResponse(int status, String message, Object result) {
+	public ApiResponse(int status, String message, T result) {
 		this.status = status;
 		this.message = message;
 		this.result = result;
