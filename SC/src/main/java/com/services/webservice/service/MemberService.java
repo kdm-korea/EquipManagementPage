7e479f00c00bd69b409d485c30d98f2dccce26d9
KDM_korea <kdm_korea@naver.com>
2020-02-13 15:35:03 +0900

    Refactor: UserDetailService를 상속받은 파트를 UserDetailService로 이동하고 MemberService에서는 나머지 로그인/로그아웃에 필요한 로직을 두었다

--- SC/src/main/java/com/services/webservice/service/MemberService.java
+++ SC/src/main/java/com/services/webservice/service/MemberService.java
@@ -1,43 +1,30 @@
 package com.services.webservice.service;
 
-import java.util.ArrayList;
-
 import javax.transaction.Transactional;
 
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.services.webservice.lib.ApiResponse;
-import com.services.webservice.service.dto.SignUp.MemberSignUpDto;
-import com.services.webservice.service.dto.SignUp.UserStudentNumChkDto;
 import com.services.webservice.domain.ERole;
-import com.services.webservice.domain.Member.Member;
 import com.services.webservice.domain.Member.MemberRepository;
 import com.services.webservice.domain.Member.Role;
 import com.services.webservice.domain.Member.RoleRepository;
-
-import java.util.List;
+import com.services.webservice.service.dto.SignUp.MemberSignUpDto;
+import com.services.webservice.service.dto.SignUp.UserStudentNumChkDto;
 
 import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
 @Service
-public class MemberService implements UserDetailsService {
-
-	@Autowired
-	private MemberRepository memberRepository;
+@AllArgsConstructor
+public class MemberService {
 
 	@Autowired
 	private RoleRepository roleRepository;
-
+	
+	@Autowired
+	private MemberRepository memberRepository;
+	
 	public Long signUp(MemberSignUpDto userDto) {
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -46,21 +33,6 @@ public class MemberService implements UserDetailsService {
 		userDto.setRoleId(userRole);
 
 		return createMember(userDto);
-	}
-
-	@Override
-	public UserDetails loadUserByUsername(String studentNum) throws UsernameNotFoundException {
-		Member memberEntity;
-		List<GrantedAuthority> authorities;
-
-		try {
-			memberEntity = memberRepository.findByStudentNum(studentNum);
-			authorities = new ArrayList<GrantedAuthority>();
-			authorities.add(new SimpleGrantedAuthority(memberEntity.getRoleId().getRole()));
-		} catch (Exception e) {
-			throw new UsernameNotFoundException(studentNum);
-		}
-		return new User(memberEntity.getStudentNum(), memberEntity.getPassword(), authorities);
 	}
 
 	public boolean studentNumChk(UserStudentNumChkDto dto) {
