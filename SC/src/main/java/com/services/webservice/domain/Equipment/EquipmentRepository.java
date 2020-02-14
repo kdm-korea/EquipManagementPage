261147146eb824ee0fafbc5e223792885b26784c
KDM_korea <kdm_korea@naver.com>
2020-02-14 17:26:43 +0900

    Feat: 기자재 데이터 셀렉 jpa 쿼리 구성

--- SC/src/main/java/com/services/webservice/domain/Equipment/EquipmentRepository.java
+++ SC/src/main/java/com/services/webservice/domain/Equipment/EquipmentRepository.java
@@ -1,7 +1,14 @@
 package com.services.webservice.domain.Equipment;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EquipmentRepository extends JpaRepository<Equipment, Long>{
-
+	@Query("SELECT p " +
+            "FROM Equipment p " +
+            "ORDER BY p.equipName DESC")
+	List<Equipment> findAllbyOrderByDesc();
 }
