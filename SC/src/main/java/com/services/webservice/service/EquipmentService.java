e122fefa548d849a88f1a891dbcf361a4ea4d6aa
KDM_korea <kdm_korea@naver.com>
2020-02-15 18:30:39 +0900

    FIx: stream을 통해 리스트 클래스 변경

--- SC/src/main/java/com/services/webservice/service/EquipmentService.java
+++ SC/src/main/java/com/services/webservice/service/EquipmentService.java
@@ -2,7 +2,6 @@ package com.services.webservice.service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,12 +14,12 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @Service
 public class EquipmentService {
-	
+
 	@Autowired
 	private EquipmentRepository equipRepository;
-	
-	public List<ResEquipListDto> getEuqipList(){
-		 return equipRepository.findAllbyOrderByDesc()
+
+	public List<ResEquipListDto> getEuqipList() {
+		return equipRepository.findAllbyOrderByDesc()
 				.stream()
 				.map(ResEquipListDto::new)
 				.collect(Collectors.toList());
