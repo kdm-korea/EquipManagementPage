363f2fd4e1b75375f4084ec83b5162441ba81e65
KDM_korea <kdm_korea@naver.com>
2020-02-16 14:17:08 +0900

    Fix: 잘못된 클래스명 수정

--- SC/src/main/java/com/services/webservice/contorller/SignInUp/LoginController.java
+++ SC/src/main/java/com/services/webservice/contorller/SignInUp/LoginController.java
@@ -1,24 +1,34 @@
 package com.services.webservice.contorller.SignInUp;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Controller
-public class LogintController {
+public class LoginController {
 
 	@GetMapping("/")
 	public String main() {
 		return "index";
 	}
 
+//	@PostMapping("login")
+//	public void login(HttpSession session) {
+//		System.out.println("============================");
+//		session.setAttribute("studentNum", "2019631001");
+//	}
+	
 	@GetMapping("/logout")
 	public String logout() {
 		
 		return "redirect:/";
 	}
+	
 	// 접근 거부 페이지
 	@GetMapping("/user/denied")
 	public String dispDenied() {
