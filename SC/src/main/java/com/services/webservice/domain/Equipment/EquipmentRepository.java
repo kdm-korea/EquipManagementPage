9db16781f5d0db42e1895d73cbbd82cd6f30a24c
KDM_korea <kdm_korea@naver.com>
2020-02-20 14:49:24 +0900

    Fix: 사용 가능한 장비들의 리스트를 가져오는 쿼리문

--- SC/src/main/java/com/services/webservice/domain/Equipment/EquipmentRepository.java
+++ SC/src/main/java/com/services/webservice/domain/Equipment/EquipmentRepository.java
@@ -4,11 +4,19 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.services.webservice.domain.EState;
 
 public interface EquipmentRepository extends JpaRepository<Equipment, Long>{
+	@Modifying
 	@Query("SELECT p " +
             "FROM Equipment p " +
+			"WHERE p.isAvailable = 'true'"+
             "ORDER BY p.equipName DESC")
 	List<Equipment> findAllbyOrderByDesc();
+	
+	Equipment findByEquipNum(String equipNum);
 }
