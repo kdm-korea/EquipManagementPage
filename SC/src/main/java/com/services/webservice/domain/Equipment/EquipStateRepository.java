40d997baa3715f88705ee7cce2de0f3a7a53615f
KDM_korea <kdm_korea@naver.com>
2020-02-20 14:45:48 +0900

    Feat: 상태 select문

--- SC/src/main/java/com/services/webservice/domain/Equipment/EquipStateRepository.java
+++ SC/src/main/java/com/services/webservice/domain/Equipment/EquipStateRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EquipStateRepository extends JpaRepository<EquipState, Long>{
 
+	EquipState findByState(String state);
 }
