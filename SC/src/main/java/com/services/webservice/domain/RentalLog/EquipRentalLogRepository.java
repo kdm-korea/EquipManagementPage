f79b1791aec44c3bf01e58f41210b2fdf18bf182
KDM_korea <kdm_korea@naver.com>
2020-02-20 14:47:20 +0900

    Feat: 사용자가 이미 빌리고 있는 장비가 있는지 체크하는 쿼리문

--- SC/src/main/java/com/services/webservice/domain/RentalLog/EquipRentalLogRepository.java
+++ SC/src/main/java/com/services/webservice/domain/RentalLog/EquipRentalLogRepository.java
@@ -1,7 +1,23 @@
 package com.services.webservice.domain.RentalLog;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EquipRentalLogRepository extends JpaRepository<EquipRentalLog, Long> {
-
+//	@Modifying
+//	@Query(value = "Select * from EquipRentalLog "
+//							+ "where select * from Equipment "
+//												+ "where equipNum=:equipNum")
+	
+	@Modifying
+	@Transactional
+	@Query(value = "Select count(p) from EquipRentalLog p "
+			+ "where p.equipId.equipName = :equipName and p.memberId.studentNum = :studentNum")
+	Long findByMemberRentalSameEquip(@Param("studentNum") String studentNum, @Param("equipName") String equipName);
 }
