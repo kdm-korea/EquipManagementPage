a3a2f21e3c65c23cdcfdea8e8c5c331ea54c3075
KDM_korea <kdm_korea@naver.com>
2020-02-24 21:53:49 +0900

    Fix: 쿼리문 이름 수정

--- SC/src/main/java/com/services/webservice/domain/RentalLog/EquipRentalLogRepository.java
+++ SC/src/main/java/com/services/webservice/domain/RentalLog/EquipRentalLogRepository.java
@@ -1,23 +1,16 @@
 package com.services.webservice.domain.RentalLog;
 
-import java.util.List;
-
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface EquipRentalLogRepository extends JpaRepository<EquipRentalLog, Long> {
-//	@Modifying
-//	@Query(value = "Select * from EquipRentalLog "
-//							+ "where select * from Equipment "
-//												+ "where equipNum=:equipNum")
-	
-//	@Modifying
 	@Transactional
 	@Query("Select count(p) from EquipRentalLog p "
-			+ "where p.equipId.equipName = :equipName and p.memberId.studentNum = :studentNum")
-	int findByMemberRentalSameEquip(@Param("studentNum") String studentNum, @Param("equipName") String equipName);
+			+ "where p.equipId.equipName = :equipName "
+			+ "and p.memberId.studentNum = :studentNum "
+			+ "and p.isOverdue = false")
+	int findByMemberRentalSameEquipCount(@Param("studentNum") String studentNum, @Param("equipName") String equipName);
 }
