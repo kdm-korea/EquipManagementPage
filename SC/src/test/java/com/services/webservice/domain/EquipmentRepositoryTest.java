2b13951c12309365e47b66abf1214d94c76d99ad
KDM_korea <kdm_korea@naver.com>
2020-03-02 18:44:13 +0900

    Test: 렌탈로그 데이버베이스에 렌탈쿼리 추가 테스트

--- SC/src/test/java/com/services/webservice/domain/EquipmentRepositoryTest.java
+++ SC/src/test/java/com/services/webservice/domain/EquipmentRepositoryTest.java
@@ -3,6 +3,7 @@ package com.services.webservice.domain;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,6 +19,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.services.webservice.domain.Equipment.Equipment;
 import com.services.webservice.domain.Equipment.EquipmentRepository;
+import com.services.webservice.domain.RentalLog.EquipRentalLogRepository;
+import com.services.webservice.service.MemberService.EquipService.MemberEquipService;
+import com.services.webservice.service.dto.Equip.Request.ReqEquipRentalDto;
+import com.services.webservice.service.dto.Equip.Request.ReqEquipReturnDto;
 import com.services.webservice.service.dto.Equip.Response.ResEquipListDto;
 
 @ExtendWith(SpringExtension.class)
@@ -27,29 +32,21 @@ public class EquipmentRepositoryTest {
 
 	@Autowired
 	private EquipmentRepository equipRepo;
+	
+	@Autowired
+	private EquipRentalLogRepository equipRentalRepo;
+	
+	@Autowired
+	private MemberEquipService equipService;
 
 	@Test
 	public void compareEquipTableData() {
 		List<Equipment> dto = equipRepo.findAllbyOrderByDesc();
-//		List<ResEquipListDto> d = new ArrayList<ResEquipListDto>();
-//
-//		for (int idx = 0; idx < dto.size(); idx++) {
-//			d.add(new ResEquipListDto(dto.get(idx)));
-//		}
+//		
 		List<ResEquipListDto> list = dto
 				.stream()
 				.map(ResEquipListDto::new)
 				.collect(Collectors.toList());
-
-		System.out.println("========================================================");
-		System.out.println("========================================================");
-		System.out.println(dto.get(0)
-				.getEquipStateId()
-				.getState()
-				.toString());
-		System.out.println(list.get(0).getEquipName());
-		System.out.println("========================================================");
-		System.out.println("========================================================");
 
 		assertEquals(list.get(0).getEquipName(), "VR기기");
 	}
@@ -57,10 +54,21 @@ public class EquipmentRepositoryTest {
 	@Test
 	public void compareValueEuqipBasicQuery() {
 		List<Equipment> list = equipRepo.findAllbyOrderByDesc();
-		Stream<Equipment> stream = list.stream();
-
-		stream.forEach(m -> System.out.println(m.getEquipName()));
 
 		assertEquals(list.get(0).getEquipName(), "VR기기");
+	}
+	
+	@Test
+	public void insertEquipRentQuery() {
+		ReqEquipRentalDto rentaldto = ReqEquipRentalDto.builder()
+				.studentNum("2019631001")
+				.equipName("VR기기")
+				.equipNum("1234")
+				.reason("필요함")
+				.build();
+		
+		equipService.saveEquipRentalLog(rentaldto);
+		
+		assertEquals(equipRentalRepo.findAll().get(0).getReason(),"필요함");
 	}
 }
