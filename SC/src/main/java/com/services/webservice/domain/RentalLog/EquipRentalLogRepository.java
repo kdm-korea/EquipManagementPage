80c51c2c3bdf3247ffc584054a10b4e82d51fa38
KDM_korea <kdm_korea@naver.com>
2020-03-03 18:45:09 +0900

    Fix: 트랜젝션 제거 메소드에서 트랜젝션을 적용함

--- SC/src/main/java/com/services/webservice/domain/RentalLog/EquipRentalLogRepository.java
+++ SC/src/main/java/com/services/webservice/domain/RentalLog/EquipRentalLogRepository.java
@@ -1,5 +1,7 @@
 package com.services.webservice.domain.RentalLog;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,17 +9,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface EquipRentalLogRepository extends JpaRepository<EquipRentalLog, Long> {
-	@Transactional
 	@Query("Select count(p) from EquipRentalLog p "
 			+ "where p.equipId.equipName = :equipName "
 			+ "and p.memberId.studentNum = :studentNum "
 			+ "and p.isOverdue = false")
-	int findByMemberRentalSameEquipCount(@Param("studentNum") String studentNum, @Param("equipName") String equipName);
+	int findbyMemberRentalSameEquipCount(@Param("studentNum") String studentNum, @Param("equipName") String equipName);
 	
-	@Transactional
 	@Query("Select p From EquipRentalLog p "
 			+ "Where p.equipId.equipNum = :equipNum "
 			+ "and p.memberId.studentNum = :studentNum "
-			+ "and p.isOverdue = false")
-	EquipRentalLog findByMemberRentalSameEquip(@Param("studentNum") String studentNum, @Param("equipNum") String equipNum);
+			+ "and p.realReturnTime = null")
+	List<EquipRentalLog> findbyMemberRentalSameEquip(@Param("studentNum") String studentNum, @Param("equipNum") String equipNum);
 }
