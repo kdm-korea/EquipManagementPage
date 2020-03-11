5064b1ec26bbc916c2590e28bd5894e16277f96b
KDM_korea <kdm_korea@naver.com>
2020-03-11 22:13:38 +0900

    Feat: Computer controller frame

--- SC/src/main/java/com/services/webservice/controller/Member/ComputerController.java
+++ SC/src/main/java/com/services/webservice/controller/Member/ComputerController.java
@@ -1,5 +1,40 @@
 package com.services.webservice.controller.Member;
 
-public class ComputerController {
+import java.security.Principal;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/member/computer")
+public class ComputerController {
+	
+	@GetMapping("/list")
+	public String computer(HttpSession session, Model model) {
+		return "Member/memberComputer";
+	}
+
+	@PostMapping("/rent")
+	public String computerRent(HttpSession session) {
+		return "";
+	}
+	
+	@PostMapping("return")
+	public String computerReturn(HttpSession session) {
+		return "";
+	}
+	
+	@GetMapping("/chkAlreadyHaveComputer")
+	public String chkAlreadyHaveComputer(Principal principal, Model model) {
+		return "";
+	}
+	
 }
