af863feb104a5e97ce132371bae4ed503854466d
KDM_korea <kdm_korea@naver.com>
2020-03-12 21:10:11 +0900

    Fix: pk를 기본으로 하는 쿼리로 변경

--- SC/src/main/java/com/services/webservice/service/MemberService/EquipService/EquipService.java
+++ SC/src/main/java/com/services/webservice/service/MemberService/EquipService/EquipService.java
@@ -12,13 +12,11 @@ import com.services.webservice.domain.EState;
 import com.services.webservice.domain.Equipment.EquipStateRepository;
 import com.services.webservice.domain.Equipment.EquipmentRepository;
 import com.services.webservice.domain.Member.MemberRepository;
-import com.services.webservice.domain.RentalLog.EquipRentalLog;
 import com.services.webservice.domain.RentalLog.EquipRentalLogRepository;
+import com.services.webservice.service.dto.Equip.Dao.EquipRentalDao;
 import com.services.webservice.service.dto.Equip.Request.ReqEquipRentalDto;
 import com.services.webservice.service.dto.Equip.Request.ReqEquipReturnDto;
 import com.services.webservice.service.dto.Equip.Response.ResEquipListDto;
-import com.services.webservice.service.dto.Equip.Save.SaveEquipRentalDto;
-import com.services.webservice.service.dto.Equip.Save.SaveEquipReturnDto;
 
 import lombok.AllArgsConstructor;
 
@@ -46,56 +44,38 @@ public class EquipService {
 				.collect(Collectors.toList());
 	}
 
+	@Transactional
 	public void equipRent(ReqEquipRentalDto dto) throws NullPointerException {
 		// 이 사람이 이 물건과 같은 물건을 빌린 적이 있는지
-		if (equipLogRepo.findbyMemberRentalSameEquipCount(dto.getStudentNum(), dto.getEquipName()) > 0) {
+		if (equipLogRepo.findbyMemberRentalSameEquipCount(dto.getMemberId(), dto.getEquipId()) > 0) {
 			// Message: 이미 같은 기자재를 빌리는 중입니다.		
 		} 
 		else {
-			saveEquipRentalLogQuery(dto);
+			equipRentalQuery(dto);
 			//Message: 완료되었습니다.
 		}
 	}
 	
-	public void equipReturn(ReqEquipReturnDto dto) throws NullPointerException{
-		saveEquipReturnLogQuery(dto);
-	}
-	
 	@Transactional
-	private void saveEquipReturnLogQuery(ReqEquipReturnDto dto)throws NullPointerException {
-//		equipLogRepo.updateReturnEquip(memberRepo.findByStudentNum(dto.getStudentNum()), 
-//						equipRepo.findByEquipNum(dto.getEquipNum()), 
-//						dto.getRealReturnTime());
+	public void equipReturn(ReqEquipReturnDto dto) throws NullPointerException{ 
+		equipLogRepo.updateReturnEquip(dto.getMemberId(), 
+				dto.getEquipId(), 
+				dto.getRealReturnTime());
 		
-		EquipRentalLog log = equipLogRepo.findbyMemberRentalSameEquip(dto.getStudentNum(), dto.getEquipNum()).get(0);
-		
-		equipLogRepo.save(SaveEquipReturnDto.builder()
-				.memberId(log.getMemberId())
-				.equipId(log.getEquipId())
-				.rentalTime(log.getRentalTime())
-				.predictReturnTime(log.getPredictReturnTime())
-				.realReturnTime(dto.getRealReturnTime())
-				.isOverdue(log.isOverdue())
-				.reason(log.getReason())
-				.build()
-				.toEntity());
-		
-//		equipRepo.updatebyRentalEquip(dto.getEquipNum(), equipStateRepo.findByState(EState.ACTIVATE.getValue()));
+		equipRepo.updatebyRentalEquip(dto.getEquipId(), equipStateRepo.findByState(EState.ACTIVATE.getValue()));
 	}
 
 	@Transactional
-	private void saveEquipRentalLogQuery(ReqEquipRentalDto dto) {
-		equipLogRepo.save(SaveEquipRentalDto.builder()
-				.memberId(memberRepo
-						.findByStudentNum(dto.getStudentNum()))
-				.equipId(equipRepo
-						.findByEquipNum(dto.getEquipNum()))
+	private void equipRentalQuery(ReqEquipRentalDto dto) {
+		equipLogRepo.save(EquipRentalDao.builder()
+				.memberId(memberRepo.getOne(dto.getMemberId()))
+				.equipId(equipRepo.getOne(dto.getEquipId()))
 				.rentalTime(dto.getRentalTime())
 				.predictReturnTime(dto.getPredictReturnTime())
 				.reason(dto.getReason())
 				.build()
 				.toEntity());
 		
-//		equipRepo.updatebyRentalEquip(dto.getEquipNum(), equipStateRepo.findByState(EState.USE.getValue()));
+		equipRepo.updatebyRentalEquip(dto.getEquipId(), equipStateRepo.findByState(EState.USE.getValue()));
 	}
 }
