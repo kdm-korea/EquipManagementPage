7490ee63356da577cc544a240db1d36c464bb1a0
KDM_korea <kdm_korea@naver.com>
2020-03-12 21:11:34 +0900

    Fix: respose dto add pk

--- SC/src/main/java/com/services/webservice/service/dto/MemberInfo/ResMemberInfoDto.java
+++ SC/src/main/java/com/services/webservice/service/dto/MemberInfo/ResMemberInfoDto.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 @Getter
 public class ResMemberInfoDto {
 
+	private long memberid;
+	
 	private String studentNum;
 	
 	private String name;
@@ -14,6 +16,7 @@ public class ResMemberInfoDto {
 	private String phoneNumber;
 	
 	public ResMemberInfoDto(Member member) {
+		this.memberid = member.getId();
 		this.studentNum = member.getStudentNum();
 		this.name = member.getName();
 		this.phoneNumber = member.getPhoneNumber();
