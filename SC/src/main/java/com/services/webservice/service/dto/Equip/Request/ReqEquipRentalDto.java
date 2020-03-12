ccc572fbe51feb4879ea1f3db80b027d4a14ea6d
KDM_korea <kdm_korea@naver.com>
2020-03-12 21:09:13 +0900

    Fix: pk를 기본으로 하는 쿼리로 변경

--- SC/src/main/java/com/services/webservice/service/dto/Equip/Request/ReqEquipRentalDto.java
+++ SC/src/main/java/com/services/webservice/service/dto/Equip/Request/ReqEquipRentalDto.java
@@ -4,19 +4,19 @@ import java.time.LocalDateTime;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.services.webservice.library.TimeFormatter;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ReqEquipRentalDto {
+public class ReqEquipRentalDto extends TimeFormatter {
 	
-	private String studentNum;
+	private long memberId;
 	
-	private String equipName;
-	
-	private String equipNum;
+	private long equipId;
 	
 	private String reason;
 	
@@ -27,14 +27,21 @@ public class ReqEquipRentalDto {
 	private LocalDateTime predictReturnTime;
 
 	@Builder
-	public ReqEquipRentalDto(String studentNum, String equipName, String equipNum, String reason,
-			LocalDateTime rentalTime, LocalDateTime predictReturnTime) {
+	public ReqEquipRentalDto(long memberId, long equipId, String reason, LocalDateTime rentalTime,
+			LocalDateTime predictReturnTime) {
 		super();
-		this.studentNum = studentNum;
-		this.equipName = equipName;
-		this.equipNum = equipNum;
+		this.memberId = memberId;
+		this.equipId = equipId;
 		this.reason = reason;
 		this.rentalTime = rentalTime;
 		this.predictReturnTime = predictReturnTime;
+	}
+	
+	public void setRentalTime(String rentalTime) {
+		this.rentalTime = LocalDateTime.parse(rentalTime, formatter);
+	}
+	
+	public void setPredictReturnTime(String predictReturnTime) {
+		this.rentalTime = LocalDateTime.parse(predictReturnTime, formatter);
 	}
 }
