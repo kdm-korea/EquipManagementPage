3677da0721b98f3aa7971d87b48cba21db3dbf4f
KDM_korea <kdm_korea@naver.com>
2020-03-12 21:09:21 +0900

    Fix: pk를 기본으로 하는 쿼리로 변경

--- SC/src/main/java/com/services/webservice/service/dto/Equip/Request/ReqEquipReturnDto.java
+++ SC/src/main/java/com/services/webservice/service/dto/Equip/Request/ReqEquipReturnDto.java
@@ -1,6 +1,11 @@
 package com.services.webservice.service.dto.Equip.Request;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.services.webservice.library.TimeFormatter;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -8,18 +13,23 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class ReqEquipReturnDto {
-	private String studentNum;
+public class ReqEquipReturnDto extends TimeFormatter{
 	
-	private String equipNum;
+	private long memberId;
+	
+	private long equipId;
 	
 	private LocalDateTime realReturnTime;
 
 	@Builder
-	public ReqEquipReturnDto(String studentNum, String equipNum, LocalDateTime realReturnTime) {
+	public ReqEquipReturnDto(long memberId, long equipId, String realReturnTime) {
 		super();
-		this.studentNum = studentNum;
-		this.equipNum = equipNum;
-		this.realReturnTime = realReturnTime;
+		this.memberId = memberId;
+		this.equipId = equipId;
+		this.realReturnTime = LocalDateTime.parse(realReturnTime, formatter);
+	}
+		
+	public void setRealReturnTime(String realReturnTime) {
+		this.realReturnTime = LocalDateTime.parse(realReturnTime, formatter);
 	}
 }
