89775cd9e02f9d41c590760ca235e8f87403470b
KDM_korea <kdm_korea@naver.com>
2020-03-13 20:45:12 +0900

    Feat: 멤버의 패스워드 변경 쿼리

--- SC/src/main/java/com/services/webservice/domain/Member/MemberRepository.java
+++ SC/src/main/java/com/services/webservice/domain/Member/MemberRepository.java
@@ -3,9 +3,17 @@ package com.services.webservice.domain.Member;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long>{
 	List<Member> findByName(String name);
 	
 	Member findByStudentNum(String studentNum);
+	
+	@Modifying
+	@Query("Update Member m Set m.password = :pw Where m.id = :id")
+	void updatePw(@Param("pw") String pw, @Param("id")long id);
+	
 }
