6dffb5a5bda84993c84c5283eed58b77c8e2792a
KDM_korea <kdm_korea@naver.com>
2020-03-13 20:48:25 +0900

    Feat: 프런트의 PC렌탈 시 요청 dto

---
+++ SC/src/main/java/com/services/webservice/service/dto/Computer/Request/ReqComputerRentalDto.java
@@ -0,0 +1,42 @@
+package com.services.webservice.service.dto.Computer.Request;
+
+import java.time.LocalDateTime;
+
+import com.services.webservice.library.TimeFormatter;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReqComputerRentalDto extends TimeFormatter {
+	private long memberId;
+
+	private long pcId;
+
+	private LocalDateTime rentalTime;
+
+	private LocalDateTime predictReturnTime;
+
+	private String reason;
+
+	@Builder
+	public ReqComputerRentalDto(long memberId, long pcId, LocalDateTime rentalTime, LocalDateTime predictReturnTime,
+			String reason) {
+		super();
+		this.memberId = memberId;
+		this.pcId = pcId;
+		this.rentalTime = rentalTime;
+		this.predictReturnTime = predictReturnTime;
+		this.reason = reason;
+	}
+	
+	public void setRentalTime(String rentalTime) {
+		this.rentalTime = LocalDateTime.parse(rentalTime, formatter);
+	}
+
+	public void setPredictReturnTime(String predictReturnTime) {
+		this.predictReturnTime = LocalDateTime.parse(predictReturnTime, formatter);
+	}	
+}
