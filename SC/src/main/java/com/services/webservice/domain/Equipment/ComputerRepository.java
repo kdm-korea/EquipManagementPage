144dc4a9435d51e6ce0060ac75673b2626d24ebb
KDM_korea <kdm_korea@naver.com>
2020-03-13 20:54:26 +0900

    Feat:: PC렌탈 / 반납 시 PC의 상태 변경을 위한 쿼리

--- SC/src/main/java/com/services/webservice/domain/Equipment/ComputerRepository.java
+++ SC/src/main/java/com/services/webservice/domain/Equipment/ComputerRepository.java
@@ -3,7 +3,17 @@ package com.services.webservice.domain.Equipment;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ComputerRepository extends JpaRepository<Computer, Long>{
-	List<Computer> findByEquipNum(String equipNum);
+	@Query("SELECT c " +
+            "FROM Computer c " +
+			"WHERE c.isAvailable = 'true'")
+	List<Computer> findAllbyAvailable();
+	
+	@Modifying
+	@Query("Update Computer c set c.isAvailable = false, c.equipStateId = :equipState Where c.id = :pcId")
+	void updatebyRentalPC(@Param("pcId") long pcId, @Param("equipState")EquipState equipState);
 }
