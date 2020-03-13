853b3d8fb994cf55c31872462fe563a029ae7b43
KDM_korea <kdm_korea@naver.com>
2020-03-13 20:37:16 +0900

    Feat: 서비스단의 컴퓨터 리스트

--- SC/src/main/java/com/services/webservice/controller/Member/ComputerController.java
+++ SC/src/main/java/com/services/webservice/controller/Member/ComputerController.java
@@ -4,12 +4,15 @@ import java.security.Principal;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.services.webservice.service.MemberService.MemberInfo;
+import com.services.webservice.service.ComputerService.ComputerService;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -17,8 +20,20 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/member/computer")
 public class ComputerController {
 	
+	@Autowired
+	private MemberInfo memberInfo;
+	
+	@Autowired
+	private ComputerService service; 
+	
 	@GetMapping("/list")
 	public String computer(HttpSession session, Model model) {
+		if(session != null) {
+			model.addAttribute("memberInfo", memberInfo.findByMemeberInfo());
+			model.addAttribute("computerList", service.pcList());
+		}else {
+			return "redirect:/";
+		}
 		return "Member/memberComputer";
 	}
 
