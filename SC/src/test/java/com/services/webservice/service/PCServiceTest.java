57855968d6cf0df36c2f9fdbd6b747416aa8585d
KDM_korea <kdm_korea@naver.com>
2020-03-13 20:55:58 +0900

    Test: PC 관련 서비스 테스트

---
+++ SC/src/test/java/com/services/webservice/service/PCServiceTest.java
@@ -0,0 +1,120 @@
+package com.services.webservice.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.AfterTestMethod;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.services.webservice.domain.EState;
+import com.services.webservice.domain.Equipment.Computer;
+import com.services.webservice.domain.Equipment.ComputerRepository;
+import com.services.webservice.domain.Equipment.EquipStateRepository;
+import com.services.webservice.domain.RentalLog.PCRentalLog;
+import com.services.webservice.domain.RentalLog.PCRentalLogRepository;
+import com.services.webservice.service.ComputerService.ComputerService;
+import com.services.webservice.service.dto.Computer.Request.ReqComputerRentalDto;
+import com.services.webservice.service.dto.Computer.Request.ReqComputerReturnDto;
+import com.services.webservice.service.dto.Computer.Response.ResComputerListDto;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@TestInstance(Lifecycle.PER_CLASS)
+public class PCServiceTest {
+
+	@Autowired
+	private ComputerRepository pcRepo;
+	
+	@Autowired
+	private EquipStateRepository stateRepo;
+	
+	@Autowired
+	private ComputerService pcService;
+	
+	@Autowired
+	private PCRentalLogRepository pcLogRepo;
+	
+	@BeforeEach
+	public void insertpcData() {
+		pcRepo.save(Computer.builder()
+				.className("505")
+				.pcSeqNum(0)
+				.equipNum("25214")
+				.equipStateId(stateRepo.findByState(EState.ACTIVATE.getValue()))
+				.isAvailable(true)
+				.build());
+	}
+	
+	@AfterEach
+	public void deleteAll() {
+		pcLogRepo.deleteAll();
+		pcRepo.deleteAll();
+	}
+	
+	@Test
+	public void pcListTest() {
+		List<ResComputerListDto> list = pcService.pcList();
+		
+		String className = list.get(0).getClassName();
+		
+		assertEquals("505", className);
+	}
+	
+	@Test
+	public void pcRentTest() {
+		pcService.pcRent(ReqComputerRentalDto.builder()
+				.memberId(0)
+				.pcId(pcRepo.findAll().get(0).getId())
+				.rentalTime(LocalDateTime.now())
+				.predictReturnTime(LocalDateTime.now())
+				.reason("Test")
+				.build());
+
+		PCRentalLog log = pcLogRepo.findAll().get(0);
+		Computer pc = pcRepo.findAll().get(0);
+		
+		assertEquals("2019631001", log.getMemberId().getStudentNum());
+		assertEquals("25214", log.getPcId().getEquipNum());
+		assertEquals("Test", log.getReason());
+		assertEquals(EState.USE.getValue(), pc.getEquipStateId().getState());
+	}
+	
+	@Test
+	public void pcReturnTest() {
+		pcService.pcRent(ReqComputerRentalDto.builder()
+				.memberId(0)
+				.pcId(pcRepo.findAll().get(0).getId())
+				.rentalTime(LocalDateTime.now())
+				.predictReturnTime(LocalDateTime.now())
+				.reason("Test1")
+				.build());
+		
+		pcService.pcReturn(ReqComputerReturnDto.builder()
+				.memberId(0)
+				.pcId(pcRepo.findAll().get(0).getId())
+				.realReturnTime(LocalDateTime.now())
+				.build());
+		
+		PCRentalLog log = pcLogRepo.findAll().get(0);
+		Computer pc = pcRepo.findAll().get(0);
+		
+		assertNotNull(log.getRealReturnTime());
+		assertEquals(EState.ACTIVATE.getValue(), pc.getEquipStateId().getState());
+	}
+}
