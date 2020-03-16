48aa7bb873c870a7907cf22421f2aaef2bc6b0bc
KDM_korea <kdm_korea@naver.com>
2020-03-16 22:19:59 +0900

    Feat: ModelMapper을 빈즈로 만들어 생성자로 사용

--- SC/src/main/java/com/services/webservice/security/SecurityConfig.java
+++ SC/src/main/java/com/services/webservice/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.services.webservice.security;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -26,6 +27,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailServiceImpl customUserDetail;
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	};
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
