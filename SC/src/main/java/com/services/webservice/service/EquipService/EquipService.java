3b7bed98a4e5fbe2bb3154f86bfba246f3bf8eb0
KDM_korea <kdm_korea@naver.com>
2020-03-16 22:19:10 +0900

    Fix: Transaction annotation을 javax에서 springframework로 변경

--- SC/src/main/java/com/services/webservice/service/EquipService/EquipService.java
+++ SC/src/main/java/com/services/webservice/service/EquipService/EquipService.java
@@ -3,10 +3,9 @@ package com.services.webservice.service.EquipService;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.services.webservice.domain.EState;
 import com.services.webservice.domain.Equipment.EquipStateRepository;
