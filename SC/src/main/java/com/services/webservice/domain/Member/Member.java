50c29a3171e00d753e5c7303caaaef75edca66bb
KDM_korea <kdm_korea@naver.com>
2020-03-18 17:07:13 +0900

    Style:  사용하지 않는 임폴트 삭제

--- SC/src/main/java/com/services/webservice/domain/Member/Member.java
+++ SC/src/main/java/com/services/webservice/domain/Member/Member.java
@@ -1,7 +1,5 @@
 package com.services.webservice.domain.Member;
 
-import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,10 +8,7 @@ import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import com.services.webservice.domain.BaseTimeEntity;
 
