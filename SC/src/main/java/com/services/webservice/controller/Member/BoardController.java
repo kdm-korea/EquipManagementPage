bc56440d5cd870a50b62192da0cfaff9083bd696
KDM_korea <kdm_korea@naver.com>
2020-03-23 21:44:01 +0900

    Fix: Controller -> RestController

--- SC/src/main/java/com/services/webservice/controller/Member/BoardController.java
+++ SC/src/main/java/com/services/webservice/controller/Member/BoardController.java
@@ -1,60 +1,59 @@
 package com.services.webservice.controller.Member;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.services.webservice.service.BoardService.BoardService;
 import com.services.webservice.service.dto.Board.Request.ReqPostDetailDto;
 import com.services.webservice.service.dto.Board.Request.ReqSavePostDto;
+import com.services.webservice.service.dto.Board.Response.ResBoardDetailDto;
+import com.services.webservice.service.dto.Board.Response.ResBoardListDto;
 
 import lombok.AllArgsConstructor;
 
-@Controller
+@RestController
 @AllArgsConstructor
-@RequestMapping("member/board")
+@RequestMapping("member/boards")
 public class BoardController {
 
 	@Autowired
 	private BoardService service;
-
-//	public void Test() {
-//		ModelMapper modelMapper = new ModelMapper();
-//		modelMapper.
-//	}
 	
-	@GetMapping("/list")
-	public String postList(Model model) {
-		service.list();
-		return "";
+	@GetMapping()
+	public List<ResBoardListDto> postList() {
+		return service.list();
 	}
 	
 	@GetMapping("/post/{no}")
-	public String detail(@PathVariable("no")long no, Model model) {
-		service.detail(no);
-		return "";
+	public ResBoardDetailDto detail(@PathVariable("no")long no, Model model) {
+		return service.detail(no);
 	}
 
-	@PostMapping("/post/create")
-	public String create(ReqSavePostDto dto) {
-		service.save(dto);
-		return "";
+	@PostMapping("/post")
+	public Long create(ReqSavePostDto dto) {
+		return service.save(dto);
 	}
 	
-	@PostMapping("/post/edit/{no}")
-	public String update(ReqPostDetailDto dto) {
-		service.update(dto);
-		return "";
+	@PutMapping("/post/{no}")
+	public Long update(ReqPostDetailDto dto) {
+		return service.update(dto);
 	}
 	
-	@PostMapping("/post/delete/{no}")
-	public String delete(@PathVariable("no")long no) {
+	@DeleteMapping("/post/{no}")
+	public Long delete(@PathVariable("no")long no) throws Exception {
 		service.delete(no);
-		return "";
+		return no;
 	}
 }
