41ef19c0932c568f7c1a7516c06497d065b3b04e
KDM_korea <kdm_korea@naver.com>
2020-03-23 21:44:10 +0900

    Fix: Controller -> RestController

--- SC/src/main/java/com/services/webservice/controller/Member/ComputerController.java
+++ SC/src/main/java/com/services/webservice/controller/Member/ComputerController.java
@@ -1,5 +1,7 @@
 package com.services.webservice.controller.Member;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.services.webservice.service.MemberService.MemberInfo;
 import com.services.webservice.service.ComputerService.ComputerService;
 import com.services.webservice.service.dto.Computer.Request.ReqComputerRentalDto;
 import com.services.webservice.service.dto.Computer.Request.ReqComputerReturnDto;
+import com.services.webservice.service.dto.Computer.Response.ResComputerListDto;
 
 import lombok.AllArgsConstructor;
 
@@ -22,20 +26,16 @@ import lombok.AllArgsConstructor;
 public class ComputerController {
 	
 	@Autowired
-	private MemberInfo memberInfo;
-	
-	@Autowired
 	private ComputerService service; 
 	
 	@GetMapping("/list")
-	public String computer(HttpSession session, Model model) {
+	public List<ResComputerListDto> computer(HttpSession session) {
 		if(session != null) {
-			model.addAttribute("memberInfo", memberInfo.findByMemeberInfo());
-			model.addAttribute("computerList", service.pcList());
+			
 		}else {
-			return "redirect:/";
+//			return "redirect:/";
 		}
-		return "Member/memberComputer";
+		return service.pcList();
 	}
 
 	@PostMapping("/rent")
