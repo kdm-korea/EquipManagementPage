aad7e1febda9df77bd7a47318835f6973340e58c
KDM_korea <kdm_korea@naver.com>
2020-03-23 21:46:37 +0900

    FIx: 빈즈를 다른 클래스로 이동

--- SC/src/main/java/com/services/webservice/security/SecurityConfig.java
+++ SC/src/main/java/com/services/webservice/security/SecurityConfig.java
@@ -1,18 +1,13 @@
 package com.services.webservice.security;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.services.webservice.service.MemberService.UserDetailServiceImpl;
@@ -27,20 +22,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailServiceImpl customUserDetail;
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	};
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
-		return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
-	}
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
@@ -54,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				// 페이지 권한설정
 				.antMatchers("/admin/**").hasRole("ADMIN")
 				.antMatchers("/member/**").hasRole("MEMBER")
-				.antMatchers("/").permitAll()
+				.antMatchers("/**").permitAll()
 				.anyRequest().permitAll()
 			.and()
 				.formLogin().loginPage("/login")
@@ -82,6 +65,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(customUserDetail).passwordEncoder(passwordEncoder());
+		auth.userDetailsService(customUserDetail).passwordEncoder(passwordEncoder);
 	}
 }
