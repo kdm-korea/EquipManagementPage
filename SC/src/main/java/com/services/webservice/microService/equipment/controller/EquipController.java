a6b2956179ee97ee5e12e079c09d336fda6b838a
KDM_korea <kdm_korea@naver.com>
2020-03-30 14:29:14 +0900

    Refactor: 안쓰는 임폴트 삭제

--- SC/src/main/java/com/services/webservice/microService/equipment/controller/EquipController.java
+++ SC/src/main/java/com/services/webservice/microService/equipment/controller/EquipController.java
@@ -2,20 +2,12 @@ package com.services.webservice.microService.equipment.controller;
 
 import java.security.Principal;
 
-import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.services.webservice.microService.Test.service.MemberInfo;
-import com.services.webservice.microService.equipment.dto.request.ReqEquipRentalDto;
-import com.services.webservice.microService.equipment.dto.request.ReqEquipReturnDto;
 import com.services.webservice.microService.equipment.service.EquipService;
 
 import lombok.AllArgsConstructor;
