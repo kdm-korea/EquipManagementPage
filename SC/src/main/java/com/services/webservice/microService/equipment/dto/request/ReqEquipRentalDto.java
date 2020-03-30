9e4232fc33728033ee75bda07531812633551fe4
KDM_korea <kdm_korea@naver.com>
2020-03-30 14:29:55 +0900

    Feat: 유효성 추가

--- SC/src/main/java/com/services/webservice/microService/equipment/dto/request/ReqEquipRentalDto.java
+++ SC/src/main/java/com/services/webservice/microService/equipment/dto/request/ReqEquipRentalDto.java
@@ -2,7 +2,13 @@ package com.services.webservice.microService.equipment.dto.request;
 
 import java.time.LocalDateTime;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 import org.springframework.format.annotation.DateTimeFormat;
+
+import com.sun.istack.NotNull;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +19,15 @@ import lombok.Setter;
 @Setter
 public class ReqEquipRentalDto {
 
+	@NotBlank(message = "Member Blank value")
+	@Min(value = 0, message = "세션이 유효하지 않습니다" )
 	private long memberId;
 
+	@NotBlank(message = "Equip Blank Value")
+	@Min(value = 0, message = "0보다 작은 번호의 기자재는 없습니다")
 	private long equipId;
 
+	@NotBlank(message = "빌리는 이유를 적어주세요")
 	private String reason;
 
 	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
