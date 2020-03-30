20ea010b0327b93aed7990258d6a3cfdc67dcd0f
KDM_korea <kdm_korea@naver.com>
2020-03-30 14:44:06 +0900

    Feat: UserDetaiilimpl Service

---
+++ SC/src/main/java/com/services/webservice/microService/signInUp/service/UserDetailServiceImpl.java
@@ -0,0 +1,30 @@
+package com.services.webservice.microService.signInUp.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.services.webservice.domain.Member.Member;
+import com.services.webservice.domain.Member.MemberRepository;
+import com.services.webservice.security.SecurityMember;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String studentNum) throws UsernameNotFoundException {
+			Member memberEntity = memberRepository.findByStudentNum(studentNum);
+			if(memberEntity == null) {
+				throw new UsernameNotFoundException(studentNum);
+			}
+			return new SecurityMember(memberEntity);
+	}
+}
