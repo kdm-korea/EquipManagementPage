7f26c74d6fdd38185fd01f406895b1481c2c1341
KDM_korea <kdm_korea@naver.com>
2020-03-30 14:30:29 +0900

    Fix: TimeFormatter을 굳이 사용하지 않아도 json처리가 가능하여 삭제

--- SC/src/main/java/com/services/webservice/microService/equipment/dto/request/ReqEquipReturnDto.java
+++ SC/src/main/java/com/services/webservice/microService/equipment/dto/request/ReqEquipReturnDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
-public class ReqEquipReturnDto extends TimeFormatter {
+public class ReqEquipReturnDto {
 	
 	@NotNull
 	private long memberId;
