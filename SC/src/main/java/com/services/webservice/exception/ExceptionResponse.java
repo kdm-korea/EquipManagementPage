2594c2a176cf0da901c6b3f5b3808810a5403dd8
KDM_korea <kdm_korea@naver.com>
2020-03-31 23:47:41 +0900

    Fix: ExceptionResponse를 Builder를 통해 사용하도록 수정

--- SC/src/main/java/com/services/webservice/exception/ExceptionResponse.java
+++ SC/src/main/java/com/services/webservice/exception/ExceptionResponse.java
@@ -2,9 +2,12 @@ package com.services.webservice.exception;
 
 import java.time.LocalDateTime;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @Getter
 @Setter
 public class ExceptionResponse {
@@ -13,7 +16,14 @@ public class ExceptionResponse {
 	private String error;
 	private String path;
 	
-	public ExceptionResponse() {
+	@Builder
+	public ExceptionResponse(String message, String error, String path) {
+		super();
 		this.timestamp = LocalDateTime.now();
+		this.message = message;
+		this.error = error;
+		this.path = path;
 	}
+	
+	
 }
