911dbbbad281355608a6b3932732542a4f1f5735
KDM_korea <kdm_korea@naver.com>
2020-03-31 23:53:57 +0900

    Fix: Rest Response 시 규격을 맞춰 사용하기 위해 반환형 수정

--- SC/src/main/java/com/services/webservice/microService/equipment/controller/EquipRestController.java
+++ SC/src/main/java/com/services/webservice/microService/equipment/controller/EquipRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.services.webservice.library.dto.RestResponseBaseDto;
+import com.services.webservice.library.dto.RestResponse;
 import com.services.webservice.microService.equipment.dto.request.ReqEquipRentalDto;
 import com.services.webservice.microService.equipment.dto.request.ReqEquipReturnDto;
 import com.services.webservice.microService.equipment.service.EquipService;
@@ -25,14 +25,14 @@ public class EquipRestController {
 	private EquipService service;
 	
 	@PostMapping("/rent")
-	public RestResponseBaseDto equipRent(@RequestBody ReqEquipRentalDto dto) throws Exception{
+	public RestResponse<?> equipRent(@RequestBody @Valid ReqEquipRentalDto dto) throws Exception {
 		if(service.equipRent(dto)) {
-			return RestResponseBaseDto.builder()
+			return RestResponse.builder()
 				.result(true)
 				.msg("Success")
 				.build();
 		}else {
-			return RestResponseBaseDto.builder()
+			return RestResponse.builder()
 				.result(false)
 				.msg("이미 같은 종류의 기자재를 빌리고 있습니다.")
 				.build();
@@ -40,15 +40,15 @@ public class EquipRestController {
 	}
 
 	@PostMapping("/return")
-	public RestResponseBaseDto<?> equipRetrun(@RequestBody @Valid ReqEquipReturnDto dto) {
+	public RestResponse<?> equipRetrun(@RequestBody @Valid ReqEquipReturnDto dto) {
 		if(service.equipReturn(dto)) {
-			return RestResponseBaseDto.builder()
+			return RestResponse.builder()
 					.result(true)
 					.msg("Success")
 					.build();
 		}
 		else {
-			return RestResponseBaseDto.builder()
+			return RestResponse.builder()
 					.result(false)
 					.msg("빌린 물품이 없습니다.")
 					.build();
