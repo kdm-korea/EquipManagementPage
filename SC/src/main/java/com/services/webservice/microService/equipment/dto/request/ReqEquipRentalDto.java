ceb8d3603dd34dcd798ba8583420eb93c6459d2c
KDM_korea <kdm_korea@naver.com>
2020-04-01 12:49:44 +0900

    Fix: NotBlank annotation 사용 시 '0' 또한 에러처리되서 NotNull annotation으로 변경함

--- SC/src/main/java/com/services/webservice/microService/equipment/dto/request/ReqEquipRentalDto.java
+++ SC/src/main/java/com/services/webservice/microService/equipment/dto/request/ReqEquipRentalDto.java
@@ -4,11 +4,10 @@ import java.time.LocalDateTime;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
-
-import com.sun.istack.NotNull;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,11 +18,11 @@ import lombok.Setter;
 @Setter
 public class ReqEquipRentalDto {
 
-	@NotBlank(message = "Member Blank value")
+	@NotNull(message = "Member Blank value")
 	@Min(value = 0, message = "세션이 유효하지 않습니다" )
 	private long memberId;
 
-	@NotBlank(message = "Equip Blank Value")
+	@NotNull(message = "Equip Blank Value")
 	@Min(value = 0, message = "0보다 작은 번호의 기자재는 없습니다")
 	private long equipId;
 
