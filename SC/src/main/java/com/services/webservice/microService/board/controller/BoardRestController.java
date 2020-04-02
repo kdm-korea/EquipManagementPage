50bb73e52faab0a49c1412c0c2a7eb4b16c5be18
KDM_korea <kdm_korea@naver.com>
2020-04-02 13:31:33 +0900

    Feat: 게시판 RestController 생성

--- SC/src/main/java/com/services/webservice/microService/board/controller/BoardRestController.java
+++ SC/src/main/java/com/services/webservice/microService/board/controller/BoardRestController.java
@@ -1,21 +1,16 @@
 package com.services.webservice.microService.board.controller;
 
-import java.util.List;
+import javax.validation.Valid;
 
-import javax.servlet.annotation.HttpMethodConstraint;
-
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,29 +24,24 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("member/boards")
-public class BoardController {
+@RequestMapping("member/board")
+public class BoardRestController {
 
 	@Autowired
 	private BoardService service;
 	
-	@GetMapping()
-	public List<ResBoardListDto> postList() {
-		return service.list();
-	}
-	
 	@GetMapping("/post/{no}")
-	public ResBoardDetailDto detail(@PathVariable("no")long no, Model model) {
+	public ResBoardDetailDto detail(@PathVariable("no")long no) {
 		return service.detail(no);
 	}
 
 	@PostMapping("/post")
-	public Long create(ReqSavePostDto dto) {
+	public Long create(@RequestBody @Valid ReqSavePostDto dto) {
 		return service.save(dto);
 	}
 	
 	@PutMapping("/post/{no}")
-	public Long update(ReqPostDetailDto dto) {
+	public Long update(@RequestBody ReqPostDetailDto dto) {
 		return service.update(dto);
 	}
 	
