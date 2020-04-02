d9d4e274d58abbe24652316bf6fbf5c07741fee6
KDM_korea <kdm_korea@naver.com>
2020-04-02 13:33:55 +0900

    Fix: 사용하지 않는 클래스 제거

--- SC/src/main/java/com/services/webservice/microService/board/dto/response/ResBoardListDto.java
+++ SC/src/main/java/com/services/webservice/microService/board/dto/response/ResBoardListDto.java
@@ -1,13 +1,15 @@
 package com.services.webservice.microService.board.dto.response;
 
+import java.time.LocalDateTime;
+
 import com.services.webservice.library.dto.TimeFormatter;
 
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
 @Setter
-public class ResBoardListDto extends TimeFormatter {
+@Getter
+public class ResBoardListDto {
 	private long id;
 	
 	private String memberName;
