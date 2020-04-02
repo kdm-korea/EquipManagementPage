3c965e6d0b77245441435a25c87fab1b04661673
KDM_korea <kdm_korea@naver.com>
2020-04-02 13:30:42 +0900

    Fix: Jpa와 겹치는 쿼리 수정

--- SC/src/main/java/com/services/webservice/domain/RentalLog/EquipRentalLogRepository.java
+++ SC/src/main/java/com/services/webservice/domain/RentalLog/EquipRentalLogRepository.java
@@ -20,10 +20,10 @@ public interface EquipRentalLogRepository extends JpaRepository<EquipRentalLog, 
 			+ "where p.equip.equipName = :equipName "
 			+ "and p.member.id = :memberId "
 			+ "and p.realReturnTime = null")
-	int findByMemberRentalSameEquipCount(@Param("memberId") long memberId, @Param("equipName") String equipName);
+	int findbyMemberRentalSameEquipCount(@Param("memberId") long memberId, @Param("equipName") String equipName);
 	
 	@Query("Select p From EquipRentalLog p "
 			+ "Where p.member.id = :memberId "
 			+ "and p.realReturnTime = null")
-	List<EquipRentalLog> findByMemberRentalEquip(@Param("memberId") long memberId);
+	List<EquipRentalLog> findbyMemberRentalEquip(@Param("memberId") long memberId);
 }
