2894da0b163d36d03df13c3206a6281915ebde3a
KDM_korea <kdm_korea@naver.com>
2020-04-03 22:41:19 +0900

    Feat: 테이블 간 id값을 각각 넘버링하기 위해 추가

--- SC/src/main/java/com/services/webservice/domain/Board/Board.java
+++ SC/src/main/java/com/services/webservice/domain/Board/Board.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -23,7 +24,7 @@ import lombok.NoArgsConstructor;
 public class Board extends BaseTimeEntity {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@ManyToOne(targetEntity = Member.class, fetch = FetchType.EAGER)
