e317288d21d4822834d3e36d8c5131d0b47e4d81
KDM_korea <kdm_korea@naver.com>
2020-04-03 22:41:26 +0900

    Feat: 테이블 간 id값을 각각 넘버링하기 위해 추가

--- SC/src/main/java/com/services/webservice/domain/Equipment/Computer.java
+++ SC/src/main/java/com/services/webservice/domain/Equipment/Computer.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -22,7 +23,7 @@ import lombok.NoArgsConstructor;
 public class Computer extends BaseTimeEntity {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(nullable = false)
