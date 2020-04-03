38ca77830f066faede5dde917033d94e971913ec
KDM_korea <kdm_korea@naver.com>
2020-04-03 22:41:33 +0900

    Feat: 테이블 간 id값을 각각 넘버링하기 위해 추가

--- SC/src/main/java/com/services/webservice/domain/Equipment/EquipState.java
+++ SC/src/main/java/com/services/webservice/domain/Equipment/EquipState.java
@@ -3,6 +3,7 @@ package com.services.webservice.domain.Equipment;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AccessLevel;
@@ -16,7 +17,7 @@ import lombok.NoArgsConstructor;
 public class EquipState {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(nullable = false)
