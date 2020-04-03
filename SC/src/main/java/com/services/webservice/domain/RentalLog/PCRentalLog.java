76a138a159778a78cc0a17aefb3cc0142bd99fbc
KDM_korea <kdm_korea@naver.com>
2020-04-03 22:41:54 +0900

    Feat: 테이블 간 id값을 각각 넘버링하기 위해 추가

--- SC/src/main/java/com/services/webservice/domain/RentalLog/PCRentalLog.java
+++ SC/src/main/java/com/services/webservice/domain/RentalLog/PCRentalLog.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -28,7 +29,7 @@ import lombok.NoArgsConstructor;
 public class PCRentalLog extends BaseTimeEntity {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@ManyToOne(targetEntity = Member.class, fetch = FetchType.EAGER)
