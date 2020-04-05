434e8f00b82579af0234db942e98cc0e7b43cac7
KDM_korea <kdm_korea@naver.com>
2020-04-05 22:48:42 +0900

    Fix: Dto의 id값을 일관성을 위해 memberId 로 변경

--- SC/src/main/java/com/services/webservice/microService/signInUp/dto/request/ReqMemberInfoModiifedDto.java
+++ SC/src/main/java/com/services/webservice/microService/signInUp/dto/request/ReqMemberInfoModiifedDto.java
@@ -2,21 +2,23 @@ package com.services.webservice.microService.signInUp.dto.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @Setter
 @Getter
 public class ReqMemberInfoModiifedDto {
-	private long id;
+	private long memberId;
 	
 	private String password;
 	
 	private String newPassword;
 
 	@Builder
-	public ReqMemberInfoModiifedDto(long id, String password, String newPassword) {
+	public ReqMemberInfoModiifedDto(long memberId, String password, String newPassword) {
 		super();
-		this.id = id;
+		this.memberId = memberId;
 		this.password = password;
 		this.newPassword = newPassword;
 	}
