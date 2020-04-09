67761b140099fe6a973cb8bc40c4fa6fe5e40e74
KDM_korea <kdm_korea@naver.com>
2020-04-09 21:52:21 +0900

    Feat: 로그인 js 추가

--- SC/src/main/resources/static/js/sign/login.js
+++ SC/src/main/resources/static/js/sign/login.js
@@ -1,36 +1,31 @@
-var login = {
-	init : function() {
-		var _this = this;
 
-		$('#btn-login').on('click', function() {
-			// 폼 인풋 널 체크
-			// 학번 체크
-			_this.save();
-		});
-	},
-	save : function() {
-//		var data = {
-//			studentNum : $('#loginStudentNum').val()
-//		};
-//		$.ajax({
-//			type : 'POST',
-//			url : '/login',
-//			dataType : 'json',
-//			contentType : 'application/json; charset=utf-8',
-//			data : JSON.stringify(data)
-//		}).done(function() {
-//			alert('로그인 되었습니다.');
-//		});
-//		.fail(function(data, status, jqXHR) {
-//			if(status === 'parsererror'){
-//				alert('등록되지 않은 학번입니다.');	
-//			}
-//			alert(JSON.stringify(data) );
-//			alert(status);
-//			alert(jqXHR);
-//			alert('학번과 비밀번호를 확인해 주세요.');
-//		});
+const login = function(){
+	let loginStudentNum = $('#loginStudentNum').val();
+	let loginPassword = $('#loginPassword').val();
+	
+	const data = {
+			loginStudentNum : loginStudentNum,
+			loginPassword : loginPassword
 	}
-};
-
-login.init();
+	
+	$.ajax({
+		type : 'POST',
+		url : '/login',
+		dataType : 'json',
+		contentType : 'application/json; charset=utf-8',
+		data : JSON.stringify(data),
+		
+		success : function(data, textStatus){
+			if(data.redirect){
+				window.location.herf = data.redirect;
+			}
+			alert(data);
+			alert(textStatus);
+		}
+	})
+	.done(function(xhr, status, data){
+		console.log(xhr);
+		console.log(status);
+		console.log(data);
+	});
+}
