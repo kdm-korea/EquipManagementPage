d49edf404a5fe48377ab5411282b31f085641744
KDM_korea <kdm_korea@naver.com>
2020-04-09 21:51:33 +0900

    Fix: json으로 값을 받아오기 위해 생성자 어노테이션 추가

--- SC/src/main/java/com/services/webservice/microService/board/dto/request/ReqPostDetailDto.java
+++ SC/src/main/java/com/services/webservice/microService/board/dto/request/ReqPostDetailDto.java
@@ -2,8 +2,10 @@ package com.services.webservice.microService.board.dto.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @Getter
 @Setter
 public class ReqPostDetailDto {
