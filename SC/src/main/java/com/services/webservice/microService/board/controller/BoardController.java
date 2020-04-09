a8b1aecf3015b8c861e6b23868929adf4f2b4993
KDM_korea <kdm_korea@naver.com>
2020-04-09 21:50:54 +0900

    Feat: Pw  변경 컨트롤러 추가

--- SC/src/main/java/com/services/webservice/microService/board/controller/BoardController.java
+++ SC/src/main/java/com/services/webservice/microService/board/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.services.webservice.microService.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.services.webservice.microService.board.dto.response.ResBoardDetailDto;
 import com.services.webservice.microService.board.service.BoardService;
+import com.services.webservice.security.MemberDetail;
 
 import lombok.AllArgsConstructor;
 
@@ -35,5 +37,11 @@ public class BoardController {
 	public String detail(@AuthenticationPrincipal MemberDetail member, @PathVariable("no")long no, Model model) {
 		model.addAttribute("detail", service.detail(member.getUsername(), no));
 		return "board/view";
+	}
+	
+	@GetMapping("/modified/{no}")
+	public String updateView(@AuthenticationPrincipal MemberDetail member, @PathVariable("no")long no, Model model) {
+		model.addAttribute("detail", service.detail(member.getUsername(), no));
+		return "board/update";
 	}
 }
