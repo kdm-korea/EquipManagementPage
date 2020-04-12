ff2617466a43c32be6369be5625a7c3f95a5c261
KDM_korea <kdm_korea@naver.com>
2020-04-12 17:33:47 +0900

    Fix: 권한 변경

--- SC/src/main/java/com/services/webservice/security/SecurityConfig.java
+++ SC/src/main/java/com/services/webservice/security/SecurityConfig.java
@@ -35,9 +35,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				// 페이지 권한설정
-//				.antMatchers("/admin/**").hasRole("ADMIN")
-//				.antMatchers("/member/**").hasRole("MEMBER")
-				.antMatchers("/**").permitAll()
+				.antMatchers("/admin/**").hasRole("ADMIN")
+				.antMatchers("/member/**").hasRole("MEMBER")
+//				.antMatchers("/**").permitAll()
 				.anyRequest().permitAll()
 			.and()
 				.formLogin().loginPage("/login")
